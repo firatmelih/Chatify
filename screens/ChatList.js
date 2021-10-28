54a1bb2b0fdd2b218089a0caa3258237d4c68bde
firatmelih <melih.firat@ogr.sakarya.edu.tr>
2021-10-29 01:12:22 +0200

    chat screen & new contact added

--- screens/ChatList.js
+++ screens/ChatList.js
@@ -1,8 +1,55 @@
 import React from "react";
-import { Text } from "react-native";
+import { Text, View } from "react-native";
+import {
+  List,
+  Avatar,
+  Divider,
+  FAB,
+  Portal,
+  Dialog,
+  Paragraph,
+} from "react-native-paper";
 
 const ChatList = () => {
-  return <Text>This is ChatList Screen!</Text>;
+  return (
+    <View
+      style={{
+        backgroundColor: "#dfffff",
+        flex: 1,
+      }}
+    >
+      <List.Item
+        title="John Doe"
+        description="Hey this is a massage from John!"
+        left={() => <Avatar.Text label="JD" size={50} />}
+      />
+
+      <Divider inset />
+
+      <Portal>
+        <Dialog visible={true}>
+          <Dialog.Title>Contacts</Dialog.Title>
+          <Dialog.Content>
+            <List.Item
+              title="Xazard Doe"
+              description="Lovely Day!"
+              left={() => <Avatar.Text label="XD" size={50} />}
+            />
+          </Dialog.Content>
+        </Dialog>
+      </Portal>
+
+      <FAB
+        style={{
+          backgroundColor: "#00948f",
+          position: "absolute",
+          bottom: 16,
+          right: 16,
+        }}
+        icon="plus"
+      />
+    </View>
+  );
 };
 
 export default ChatList;
