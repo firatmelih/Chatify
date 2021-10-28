df07272c63da26599d0b3afc492d7a104000bd68
firatmelih <melih.firat@ogr.sakarya.edu.tr>
2021-10-29 01:12:11 +0200

    header style change

--- App.js
+++ App.js
@@ -9,6 +9,7 @@ import Register from "./screens/Register";
 import Login from "./screens/Login";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Ionicons } from "@expo/vector-icons";
+import { Provider } from "react-native-paper";
 
 const Stack = createNativeStackNavigator();
 const Tabs = createBottomTabNavigator();
@@ -26,25 +27,53 @@ const TabNavigator = () => (
       },
     })}
   >
-    <Tabs.Screen name="Chat List" component={ChatList} />
+    <Tabs.Screen
+      options={{
+        headerStyle: {
+          backgroundColor: "#00948f",
+        },
+        headerTitleAlign: "center",
+        headerTintColor: "#fff",
+        headerTitleStyle: {
+          fontWeight: "bold",
+        },
+      }}
+      name="Chat List"
+      component={ChatList}
+    />
 
-    <Tabs.Screen name="Settings" component={Settings} />
+    <Tabs.Screen
+      name="Settings"
+      options={{
+        headerStyle: {
+          backgroundColor: "#00948f",
+        },
+        headerTitleAlign: "center",
+        headerTintColor: "#fff",
+        headerTitleStyle: {
+          fontWeight: "bold",
+        },
+      }}
+      component={Settings}
+    />
   </Tabs.Navigator>
 );
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Main"
-          component={TabNavigator}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen name="Chat" component={Chat} />
-        <Stack.Screen name="Register" component={Register} />
-        <Stack.Screen name="Login" component={Login} />
-      </Stack.Navigator>
+      <Provider>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Main"
+            component={TabNavigator}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen name="Chat" component={Chat} />
+          <Stack.Screen name="Register" component={Register} />
+          <Stack.Screen name="Login" component={Login} />
+        </Stack.Navigator>
+      </Provider>
     </NavigationContainer>
   );
 };
